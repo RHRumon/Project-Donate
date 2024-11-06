e5c0046c658d02a8c842790bd72b6f05462521c0
Rakibul Hasan <rhrumon@gmail.com>
2024-11-06 13:27:27 +0600

    sabhvfecjifcbcd

--- js/window.js
+++ js/window.js
@@ -1,23 +1,23 @@
 let button = document.getElementById("navigateButton");
 
-if (window.location.pathname === "/index.html" || window.location.pathname === "/") {
+const basePath = window.location.pathname.startsWith("/Project-Donate") ? "/Project-Donate" : "";
+
+if (window.location.pathname === basePath + "/index.html" || window.location.pathname === basePath + "/") {
   // If on index.html or root path
   button.textContent = "Blog";
 
   button.addEventListener("click", function () {
-    // Use absolute path for navigation
-    window.location.href = "/blog.html";  // Change to the absolute path
+    window.location.href = basePath + "/blog.html";
     sessionStorage.setItem("secondPageVisited", true);
   });
-} else if (window.location.pathname === "/blog.html") {
+} else if (window.location.pathname === basePath + "/blog.html") {
   // If on blog.html
   if (sessionStorage.getItem("secondPageVisited")) {
     button.textContent = "Home";
   }
 
   button.addEventListener("click", function () {
-    // Use absolute path for navigation
-    window.location.href = "/index.html";  // Change to the absolute path
+    window.location.href = basePath + "/index.html";
     sessionStorage.removeItem("secondPageVisited");
   });
 }
